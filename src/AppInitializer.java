43b36873f7951addde15895f288d8f7d62c4f506
Manoj Randeni <randeni.ramr@gmail.com>
2021-07-12 01:40:14 +0530

    Update AppInitializer start method to load EditorForm

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,2 +1,26 @@
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent load = FXMLLoader.load(getClass().getResource("./view/EditorForm.fxml"));
+        Scene scene = new Scene(load);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Simple Text Editor");
+        primaryStage.getIcons().add(new Image("asset/img/SimpleTextEditorIcon.png"));
+        primaryStage.show();
+    }
 }
--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -1,5 +1,207 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import org.controlsfx.control.StatusBar;
+
 public class EditorFormController {
 
+    @FXML
+    private VBox baseVBox;
+    @FXML
+    private MenuBar mnuBar;
+    @FXML
+    private MenuItem mnuItemNew;
+    @FXML
+    private MenuItem mnuItemOpen;
+    @FXML
+    private MenuItem mnuItemSave;
+    @FXML
+    private MenuItem mnuItemSaveAs;
+    @FXML
+    private MenuItem mnuItemPrint;
+    @FXML
+    private MenuItem mnuItemPageSetup;
+    @FXML
+    private MenuItem mnuItemExit;
+    @FXML
+    private MenuItem mnuItemUndo;
+    @FXML
+    private MenuItem mnuItemRedo;
+    @FXML
+    private MenuItem mnuItemCut;
+    @FXML
+    private MenuItem mnuItemCopy;
+    @FXML
+    private MenuItem mnuItemPaste;
+    @FXML
+    private MenuItem mnuItemFind;
+    @FXML
+    private MenuItem mnuItemReplace;
+    @FXML
+    private MenuItem mnuItemSelectAll;
+    @FXML
+    private MenuItem mnuItemWrap;
+    @FXML
+    private MenuItem mnuItemZoomIn;
+    @FXML
+    private MenuItem mnuItemZoomOut;
+    @FXML
+    private MenuItem mnuItemDefaultZoom;
+    @FXML
+    private MenuItem mnuItemStatusBar;
+    @FXML
+    private MenuItem mnuItemAbout;
+    @FXML
+    private AnchorPane pneFind;
+    @FXML
+    private TextField txtFind;
+    @FXML
+    private RadioButton rdbUp;
+    @FXML
+    private ToggleGroup FindDirGrp;
+    @FXML
+    private RadioButton rdbDown;
+    @FXML
+    private CheckBox cbCaseMatchInFind;
+    @FXML
+    private Button btnFindNext;
+    @FXML
+    private Button btnFindClose;
+    @FXML
+    private AnchorPane pneReplace;
+    @FXML
+    private TextField txtFindInReplace;
+    @FXML
+    private TextField txtReplace;
+    @FXML
+    private CheckBox cbCaseMatchInReplace;
+    @FXML
+    private Button btnFindNextInReplace;
+    @FXML
+    private Button btnReplace;
+    @FXML
+    private Button btnReplaceAll;
+    @FXML
+    private Button btnReplaceClose;
+    @FXML
+    private TextArea txtEditor;
+    @FXML
+    private StatusBar stsbrBottom;
+
+    @FXML
+    private void mnuItemNew_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemOpen_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemSave_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemSaveAs_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemPrint_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemPageSetup_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemExit_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemUndo_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemRedo_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemCut_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemCopy_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemPaste_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemFind_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemReplace_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemSelectAll_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemWrap_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemZoomIn_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemZoomOut_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemDefaultZoom_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemStatusBar_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void mnuItemAbout_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnFindNext_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnFindClose_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnFindNextInReplace_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnReplace_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnReplaceAll_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnReplaceClose_onAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void txtEditor_onClicked(MouseEvent mouseEvent) {
+    }
 }
