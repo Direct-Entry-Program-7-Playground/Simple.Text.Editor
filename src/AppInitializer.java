82bfbba4d9fd05f970cad1a029a577f7e249f8f5
Manoj Randeni <randeni.ramr@gmail.com>
2021-07-12 01:59:59 +0530

    Set editor window size according to user preferences

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,11 +1,12 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 public class AppInitializer extends Application {
 
@@ -16,11 +17,29 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        Parent load = FXMLLoader.load(getClass().getResource("./view/EditorForm.fxml"));
-        Scene scene = new Scene(load);
+        // Reading preferences for the user
+        boolean isMaximized = Preferences.userRoot().node("lk.ijse.simple-text-editor").getBoolean("is-maximized", false);
+        double xPos = Preferences.userRoot().node("lk.ijse.simple-text-editor").getDouble("xPos", -1);
+        double yPos = Preferences.userRoot().node("lk.ijse.simple-text-editor").getDouble("yPos", -1);
+        double width = Preferences.userRoot().node("lk.ijse.simple-text-editor").getDouble("width", -1);
+        double height = Preferences.userRoot().node("lk.ijse.simple-text-editor").getDouble("width", -1);
+
+
+        AnchorPane root = FXMLLoader.load(getClass().getResource("./view/EditorForm.fxml"));
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Simple Text Editor");
         primaryStage.getIcons().add(new Image("asset/img/SimpleTextEditorIcon.png"));
         primaryStage.show();
+
+        // Set Editor window size
+        if (isMaximized) {
+            primaryStage.setMaximized(true);
+        } else {
+            primaryStage.setWidth(width == -1 ? root.getPrefWidth() : width);
+            primaryStage.setHeight(height == -1 ? root.getPrefHeight() : height);
+        }
+
+        
     }
 }
